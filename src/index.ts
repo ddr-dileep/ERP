a18bf5d078996695dce75448605f25d1be450296
Dileep <ddr.dileepraloti@gmail.com>
2024-09-03 11:24:58 +0530

    fix : added checking for route/method if not present

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { config } from "dotenv";
 import { databaseConnection } from "./configs/db-config";
 import rootRouter from "./routers";
+import { apiErrorResponse } from "./configs/api/api.response.config";
 
 // env and app configuration
 config();
@@ -11,6 +12,14 @@ app.use(express.json());
 const port: string = process.env.APPLICATION_PROT || "8000";
 
 app.use("/api/v1", rootRouter); // root router
+
+app.use((req, res) => {
+  res.status(404).json(
+    apiErrorResponse({
+      message: "Route or method not found",
+    })
+  );
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
