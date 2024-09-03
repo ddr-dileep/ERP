0f0f9114917ae4fcead801b0d8103ee640afc199
Dileep <ddr.dileepraloti@gmail.com>
2024-09-03 13:02:30 +0530

    feat : skills apis

--- src/models/skills.model.ts
+++ src/models/skills.model.ts
@@ -1,15 +1,30 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, { Schema, Document } from "mongoose";
+
+interface ISkill extends Document {
+  name: string;
+  createdBy: mongoose.Schema.Types.ObjectId;
+}
 
 const skillSchema: Schema = new Schema(
   {
-    name: { type: String, required: true },
+    name: {
+      type: String,
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
   { timestamps: true }
 );
 
+// Create an index on the `name` field with case-insensitive
+skillSchema.index(
+  { name: 1 },
+  { unique: true, collation: { locale: "en", strength: 2 } }
+);
+
 const skillModel = mongoose.model<ISkill>("Skill", skillSchema);
 export default skillModel;
-
-interface ISkill extends Document {
-  name: string;
-}
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import rolesRouters from "./roles.routers";
 import usreRouters from "./user.routers";
+import skillRouter from "./skills.routers";
 
 const rootRouter = Router();
 
 rootRouter.use("/user", usreRouters);
 rootRouter.use("/roles", rolesRouters);
+rootRouter.use("/skills", skillRouter);
 
 export default rootRouter;
