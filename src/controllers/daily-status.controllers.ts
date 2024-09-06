519f0d3deeffbb1e063b06c92f5a336ba3ba10a0
Dileep <ddr.dileepraloti@gmail.com>
2024-09-06 13:16:17 +0530

    feat : daily status api

--- src/controllers/daily-status.controllers.ts
+++ src/controllers/daily-status.controllers.ts
@@ -44,3 +44,111 @@ export const createDailyStatus = async (req: Request | any, res: Response) => {
     res.status(400).json(apiOtherError(error));
   }
 };
+
+export const getAllDailyStatusController = async (
+  req: Request | any,
+  res: Response
+) => {
+  try {
+    const authUsers =
+      req.user.role === "owner" ||
+      req.user.role === "hr" ||
+      req.user.role === "tech-lead" ||
+      req.user.role === "admin";
+
+    if (!authUsers) {
+      return res
+        .status(403)
+        .json(apiErrorResponse("authentication_error", "Unauthorized"));
+    }
+
+    const dailyStatuses = await dailyStatusModal
+      .find({ user: req.user.id })
+      .populate("projects leads");
+    res
+      .status(200)
+      .json(
+        apiSuccessResponse(
+          { count: dailyStatuses.length, dailyStatuses },
+          "Daily statuses fetched"
+        )
+      );
+  } catch (error) {
+    res.status(400).json(apiOtherError(error));
+  }
+};
+
+export const getDailyStatusByUserIdController = async (
+  req: Request | any,
+  res: Response
+) => {
+  try {
+    const authUsers =
+      req.user.role === "owner" ||
+      req.user.role === "hr" ||
+      req.user.role === "tech-lead" ||
+      req.user.role === "admin";
+
+    if (!authUsers) {
+      return res
+        .status(403)
+        .json(apiErrorResponse("authentication_error", "Unauthorized"));
+    }
+
+    const dailyStatuses = await dailyStatusModal.find({
+      user: req.params.userId,
+    });
+    res
+      .status(200)
+      .json(
+        apiSuccessResponse(
+          { count: dailyStatuses.length, dailyStatuses },
+          "Daily statuses fetched"
+        )
+      );
+  } catch (error) {
+    res.status(400).json(apiOtherError(error));
+  }
+};
+
+export const getMyDailyStatuses = async (req: Request | any, res: Response) => {
+  try {
+    const dailyStatuses = await dailyStatusModal.find({
+      user: req.user.id,
+    });
+    res
+      .status(200)
+      .json(
+        apiSuccessResponse(
+          { count: dailyStatuses.length, dailyStatuses },
+          "Daily statuses fetched"
+        )
+      );
+  } catch (error) {
+    res.status(400).json(apiOtherError(error));
+  }
+};
+
+export const getOneDailyStatusById = async (req: Request, res: Response) => {
+  try {
+    const status = await dailyStatusModal
+      .findById(req.params.id)
+      .populate({ path: "leads", select: "_id name email " })
+      .populate({ path: "projects", select: "_id name startDate client" })
+      .populate({
+        path: "user",
+        select: "_id name email",
+        populate: { path: "role", select: "_id name" },
+      });
+    if (!status) {
+      return res
+        .status(404)
+        .json(apiErrorResponse("not_found", "Status not found"));
+    }
+    res
+      .status(200)
+      .json(apiSuccessResponse({ status }, "Daily status fetched"));
+  } catch (error) {
+    res.status(400).json(apiOtherError(error));
+  }
+};
--- src/routers/daily-status.router.ts
+++ src/routers/daily-status.router.ts
@@ -1,6 +1,12 @@
 import { Router } from "express";
 import { authMiddleware } from "../utils/token";
-import { createDailyStatus } from "../controllers/daily-status.controllers";
+import {
+  createDailyStatus,
+  getAllDailyStatusController,
+  getDailyStatusByUserIdController,
+  getMyDailyStatuses,
+  getOneDailyStatusById,
+} from "../controllers/daily-status.controllers";
 import { dailyStatusCreateMiddleware } from "../middlewares/daily-status.middlewares";
 
 const dailyStatusRouter = Router();
@@ -12,3 +18,15 @@ dailyStatusRouter.post(
   authMiddleware,
   createDailyStatus
 );
+
+dailyStatusRouter.get("/get-all", authMiddleware, getAllDailyStatusController);
+
+dailyStatusRouter.get(
+  "/get-status/:userId",
+  authMiddleware,
+  getDailyStatusByUserIdController
+);
+
+dailyStatusRouter.get("/get-my-status", authMiddleware, getMyDailyStatuses);
+
+dailyStatusRouter.get("/get-one/:id", authMiddleware, getOneDailyStatusById);
